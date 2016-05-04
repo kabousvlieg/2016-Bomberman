8a8f130bf0796d32b5695a072904beb5258827f7
Kobus Coetzee <kobus@aselsan.local>
2016-05-04 22:49:05 +0200

    MDP engine will now calculate best movement direction

--- Reference Bot/Reference/Strategies/MDP/MDPStrategy.cs
+++ Reference Bot/Reference/Strategies/MDP/MDPStrategy.cs
@@ -18,6 +18,8 @@ namespace Reference.Strategies.MDP
     public class MdpStrategy : IStrategy
     {
         private GameMap _gameMap;
+        private PlayerPosition _playerPosition;
+
         private readonly int WallValue = 10;
         private readonly int PowerUpValue = 100;
         private readonly int SuperPowerUpValue = 200;
@@ -25,7 +27,7 @@ namespace Reference.Strategies.MDP
         private readonly int BombValue = -100;
 
 
-        enum MdpTypes
+        private enum MdpTypes
         {
             Bomb,
             Me,
@@ -37,27 +39,87 @@ namespace Reference.Strategies.MDP
             Path
         }
 
-        struct MdpBlock
+        private struct MdpBlock
         {
             public MdpTypes type;
             public int value;
             public bool validValue;
         }
 
+        struct PlayerPosition
+        {
+            public int x;
+            public int y;
+        }
+
         public GameCommand ExecuteStrategy(GameMap gameMap, char playerKey)
         {
             this._gameMap = gameMap;
             //var nodeMap = new NodeMap(gameMap, playerKey);
-            MdpBlock[,] MdpMap = new MdpBlock[gameMap.MapHeight + 1, gameMap.MapWidth + 1];
+            MdpBlock[,] MdpMap = new MdpBlock[gameMap.MapWidth + 1, gameMap.MapHeight + 1];
             
             DrawMap(playerKey);
             AssignValues(playerKey, MdpMap);
-
-            //Calculate MDP
             CalculateMdp(gameMap, MdpMap);
-
+            var bestMove = CalculateBestMoveFromMdp(MdpMap);
+            OverrideMdpMoveWithRuleEngine(gameMap, ref bestMove);
             DrawMdpMap(MdpMap);
-            return GameCommand.DoNothing;
+            return bestMove;
+        }
+
+        private void OverrideMdpMoveWithRuleEngine(GameMap gameMap, ref GameCommand bestMove)
+        {
+            //Check for survival
+                //Will our bestMdpMove move into explosion
+                //Will our bestMdpMove move into range of bomb
+
+            //Check if we can blow up the enemy
+            //Check if we can plant a bomb
+
+            //bestMove = GameCommand.DoNothing;
+        }
+
+        private GameCommand CalculateBestMoveFromMdp(MdpBlock[,] mdpMap)
+        {
+            var largestMdpValue = int.MinValue;
+            var bestMove = GameCommand.DoNothing;
+            if (_playerPosition.x > 1)
+            {
+                if (mdpMap[_playerPosition.x - 1, _playerPosition.y].validValue &&
+                    mdpMap[_playerPosition.x - 1, _playerPosition.y].value > largestMdpValue)
+                {
+                    largestMdpValue = mdpMap[_playerPosition.x - 1, _playerPosition.y].value;
+                    bestMove = GameCommand.MoveLeft;
+                }
+            }
+            if (_playerPosition.x < _gameMap.MapWidth)
+            {
+                if (mdpMap[_playerPosition.x + 1, _playerPosition.y].validValue &&
+                    mdpMap[_playerPosition.x + 1, _playerPosition.y].value > largestMdpValue)
+                {
+                    largestMdpValue = mdpMap[_playerPosition.x + 1, _playerPosition.y].value;
+                    bestMove = GameCommand.MoveRight;
+                }
+            }
+            if (_playerPosition.y > 1)
+            {
+                if (mdpMap[_playerPosition.x, _playerPosition.y - 1].validValue &&
+                    mdpMap[_playerPosition.x, _playerPosition.y - 1].value > largestMdpValue)
+                {
+                    largestMdpValue = mdpMap[_playerPosition.x, _playerPosition.y - 1].value;
+                    bestMove = GameCommand.MoveUp;
+                }
+            }
+            if (_playerPosition.y < _gameMap.MapHeight)
+            {
+                if (mdpMap[_playerPosition.x, _playerPosition.y + 1].validValue &&
+                    mdpMap[_playerPosition.x, _playerPosition.y + 1].value > largestMdpValue)
+                {
+                    largestMdpValue = mdpMap[_playerPosition.x, _playerPosition.y + 1].value;
+                    bestMove = GameCommand.MoveDown;
+                }
+            }
+            return bestMove;
         }
 
         private void CalculateMdp(GameMap gameMap, MdpBlock[,] MdpMap)
@@ -135,7 +197,7 @@ namespace Reference.Strategies.MDP
                         }
                     }
                 }
-                DrawMdpMap(MdpMap);
+                //DrawMdpMap(MdpMap);
             } while (stillNotDone);
         }
 
@@ -202,7 +264,11 @@ namespace Reference.Strategies.MDP
                         else if (block.Entity is PlayerEntity)
                         {
                             if ((block.Entity as PlayerEntity).Key == playerKey)
+                            {
                                 MdpMap[x, y].type = MdpTypes.Me;
+                                _playerPosition.x = x;
+                                _playerPosition.y = y;
+                            }
                             else
                                 MdpMap[x, y].type = MdpTypes.OtherPlayer;
                             MdpMap[x, y].validValue = false;
@@ -282,148 +348,3 @@ namespace Reference.Strategies.MDP
         }
     }
 }
-
-
-//namespace Reference.Strategies.AStar
-//{
-//    public class AStarStrategy : IStrategy
-//    {
-//        private GameMap _gameMap;
-//        public GameCommand ExecuteStrategy(GameMap gameMap, char playerKey)
-//        {
-//            this._gameMap = gameMap;
-//            var nodeMap = new NodeMap(gameMap, playerKey);
-
-//            return FindBestCommand(nodeMap);
-//        }
-
-//        GameCommand FindBestCommand(NodeMap nodeMap)
-//        {
-//            var path = FindBestPath(nodeMap);
-
-//            for (int y = 1; y <= _gameMap.MapHeight; y++)
-//            {
-//                for (int x = 1; x <= _gameMap.MapWidth; x++)
-//                {
-//                    var block = _gameMap.GetBlockAtLocation(x, y);
-//                    var node = path.FirstOrDefault(n => n.Location.X == x && n.Location.Y == y);
-//                    if (node != null)
-//                    {
-//                        Console.Write(".");
-//                    }
-//                    else if (block.Entity != null)
-//                    {
-//                        if (block.Entity is DestructibleWallEntity) Console.Write("#");
-//                        else if (block.Entity is PlayerEntity) Console.Write((block.Entity as PlayerEntity).Key);
-//                        else Console.Write("*");
-
-//                    }
-//                    else
-//                    {
-//                        Console.Write(" ");
-//                    }
-//                }
-//                Console.WriteLine();
-//            }
-
-//            var inExplosionRange = nodeMap.IsInExplosionRange(nodeMap.PlayerNode);
-
-//            if (!inExplosionRange && nodeMap.PlayerBombs.Any(x => x.BombTimer > 2))
-//            {
-//                return GameCommand.TriggerBomb;
-//            }
-
-//            if (!(path[0].NodeEntity is DestructibleWallEntity) || inExplosionRange)
-//            {
-//                Console.WriteLine("Executing move command");
-//                return GetMovementCommand(nodeMap.PlayerNode, path[0]);
-//            }
-
-//            if (path[0].NodeEntity is DestructibleWallEntity && nodeMap.PlayerBombs.Count < 1)
-//            {
-//                Console.WriteLine("Executing place bomb command");
-//                return GameCommand.PlaceBomb;
-//            }
-
-//            return GameCommand.DoNothing;
-//        }
-
-//        IList<Node> FindBestPath(NodeMap nodeMap)
-//        {
-//            var startNode = nodeMap.PlayerNode;
-
-//            var endNode = nodeMap.Nodes.Where(x => x.NodeEntity is IPowerUp).OrderBy(n => DistanceFromPlayer(nodeMap.PlayerNode, n)).FirstOrDefault();
-
-//            if (endNode == null)
-//            {
-//                var tempPath = Path.Combine(Path.GetTempPath(), _gameMap.MapSeed + "_" + nodeMap.Player.Key + ".state");
-//                if (!File.Exists(tempPath))
-//                    File.Create(tempPath).Close();
-
-//                var botState = File.ReadAllText(tempPath);
-//                if (!String.IsNullOrWhiteSpace(botState))
-//                {
-//                    var state = JsonConvert.DeserializeObject<BotState>(botState);
-//                    if (state != null)
-//                    {
-//                        Console.WriteLine("Targeting Previous Node " + state.PreviousNodeLocation);
-//                        endNode = nodeMap.Nodes.FirstOrDefault(n => n.Location.IsSameLocation(state.PreviousNodeLocation) && n.NodeEntity != null);
-//                    }
-//                }
-
-//                if (endNode == null)
-//                {
-//                    var furthestWall = nodeMap.Nodes.Where(x => x.NodeEntity is DestructibleWallEntity)
-//                        .OrderBy(n => DistanceFromPlayer(nodeMap.PlayerNode, n))
-//                        .LastOrDefault();
-
-//                    if (furthestWall != null)
-//                    {
-//                        endNode = furthestWall;
-//                        var state = new BotState()
-//                        {
-//                            PreviousNodeLocation = furthestWall.Location
-//                        };
-
-//                        File.WriteAllText(tempPath, JsonConvert.SerializeObject(state));
-//                    }
-//                }
-//            }
-
-//            if (endNode == null)
-//            {
-//                endNode = nodeMap.Nodes[new Random().Next(nodeMap.Nodes.Count)];
-//            }
-
-//            return new PathFinder(nodeMap).FindBestPath(startNode, endNode);
-//        }
-
-//        GameCommand GetMovementCommand(Node nodeA, Node nodeB)
-//        {
-//            var locA = nodeA.Location;
-//            var locB = nodeB.Location;
-
-//            if (locA.X == locB.X)
-//            {
-//                return locA.Y > locB.Y ? GameCommand.MoveUp : GameCommand.MoveDown;
-//            }
-
-//            if (locA.Y == locB.Y)
-//            {
-//                return locA.X > locB.X ? GameCommand.MoveLeft : GameCommand.MoveRight;
-//            }
-
-//            return GameCommand.DoNothing;
-//        }
-
-//        int DistanceFromPlayer(Node p, Node n)
-//        {
-//            var x1 = p.Location.X;
-//            var x2 = n.Location.X;
-//            var y1 = p.Location.Y;
-//            var y2 = p.Location.Y;
-
-//            return (int)Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
-//        }
-//    }
-//}
